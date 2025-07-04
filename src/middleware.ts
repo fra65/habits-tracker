e266ae1cc0e9d128a04dc861b8243c55a6918a6e
Francesco Villani <francivillani06@gmail.com>
2025-07-04 10:40:19 +0200

    feat: reset middleware setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,19 @@
-export { auth as middleware } from "@/lib/auth"
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
+export function middleware(request: NextRequest) {
 
-// export function middleware(request: NextRequest) {
+  // Permetti la richiesta
+  return NextResponse.next();
 
-//   // Permetti la richiesta
-//   return NextResponse.next();
+}
 
-// }
+// Configura le rotte dove applicare il middleware (opzionale)
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
 
-// // Configura le rotte dove applicare il middleware (opzionale)
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
+// export { auth as middleware } from "@/lib/auth"
+
