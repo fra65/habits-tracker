0a78d2f2423db1ad8014d34211e5aea16fe0be94
Francesco Villani <francivillani06@gmail.com>
2025-07-14 16:09:52 +0200

    feat: update profile. Not graphic and not validation, just logic implemented

---
+++ src/app/update-profile/page.tsx
@@ -0,0 +1,6 @@
+import { UpdateForm } from "@/modules/profile/components/forms/updateForm";
+
+
+export default function Page() {
+  return <UpdateForm />
+}
--- src/modules/profile/api/updateProfile.ts
+++ src/modules/profile/api/updateProfile.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import { ProfileUpdateOutput } from "../schema/ProfileUpdateOutputSchema";
-import { ProfileUpdateInput } from "../schema/ProfileUpdateInputSchema";
+// import { ProfileUpdateInput } from "../schema/ProfileUpdateInputSchema";
 
-export default async function updateProfile(data: ProfileUpdateInput): Promise<ProfileUpdateOutput | null | undefined> {
+export default async function updateProfile(data: any): Promise<ProfileUpdateOutput | null | undefined> {
 
     try {
     const response = await axios.put("/api/profiles", data);
